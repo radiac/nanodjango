fb089271f5f6c0a78b01fd0f380bceae79a4280a
Richard Terry <git@radiac.net>
2024-10-13 21:30:03 +0100

    Move test app into fixture

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,29 +3,27 @@ from django.test import Client
 
 from nanodjango import Django
 
-app = Django()
 
+@pytest.fixture(scope="session", autouse=True)
+def nanodjango_app():
+    app = Django()
 
-# No parameters
-@app.route("/")
-def view(request):
-    return "Hello"
+    # No parameters
+    @app.route("/")
+    def view(request):
+        return "Hello"
 
+    # path() parameters
+    @app.route("/<int:pk>/")
+    def view(request, pk):
+        return f"Hello {pk}"
 
-# path() parameters
-@app.route("/<int:pk>/")
-def view(request, pk):
-    return f"Hello {pk}"
+    # re_path() parameters
+    @app.route("/(?P<slug>[a-z])/", re=True)
+    def view(request, slug):
+        return f"Hello {slug}"
 
-
-# re_path() parameters
-@app.route("/(?P<slug>[a-z])/", re=True)
-def view(request, slug):
-    return f"Hello {slug}"
-
-
-"""
-"""
+    return app
 
 
 @pytest.fixture(scope="module", autouse=True)
