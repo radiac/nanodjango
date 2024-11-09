f1b5e5d95d2519b826e9004f998108f876c4eac8
Skycode <skycode.src@gmail.com>
2024-11-09 02:41:10 +0100

    Refacto function string-view in  view

--- nanodjango/views.py
+++ nanodjango/views.py
@@ -8,19 +8,19 @@ def string_view(fn):
     Wrapper to automatically convert the response from a view function into an
     HttpResponse to support returning a string.
     """
-    if inspect.iscoroutinefunction(fn):
 
+    def make_response(response):
+        if isinstance(response, HttpResponse):
+            return response
+        return HttpResponse(response)
+
+    if inspect.iscoroutinefunction(fn):
         async def django_view(request, **kwargs):
             response = await fn(request)
-            if isinstance(response, HttpResponse):
-                return response
-            return HttpResponse(response)
+            return make_response(response)
     else:
-
         def django_view(request, **kwargs):
             response = fn(request, **kwargs)
-            if isinstance(response, HttpResponse):
-                return response
-            return HttpResponse(response)
+            return make_response(response)
 
     return django_view
