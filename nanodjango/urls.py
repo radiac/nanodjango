67f71ff3fdd808627aff0332e7357f4a7b118a09
Fabian Binz <fabian.binz@gmail.com>
2024-11-20 22:50:07 +0100

    Integrate DJP

--- nanodjango/urls.py
+++ nanodjango/urls.py
@@ -1,5 +1,6 @@
 # Will be populated at runtime
 from django.urls.resolvers import URLPattern, URLResolver
+import djp
 
 
-urlpatterns: list[URLPattern | URLResolver] = []
+urlpatterns: list[URLPattern | URLResolver] = djp.urlpatterns()
