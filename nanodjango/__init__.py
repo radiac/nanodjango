24e73c1682e16768ed3dcc4ecb60998689f67390
Richard Terry <git@radiac.net>
2026-02-22 20:40:42 +0000

    Version 0.15.0

--- nanodjango/__init__.py
+++ nanodjango/__init__.py
@@ -6,5 +6,5 @@ from .defer import defer
 # Register pluggy hook
 hookimpl = pluggy.HookimplMarker("nanodjango")
 
-__version__ = "0.14.0"
+__version__ = "0.15.0"
 __all__ = ["Django", "defer", "hookimpl"]
